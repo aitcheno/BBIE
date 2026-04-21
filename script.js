5023aabbfa4d9c6cae24cf423d394a778b3583c9
aitcheno <162404808+aitcheno@users.noreply.github.com>
2026-04-21 15:04:07 +1000

    play pause final fix i pray

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("JS loaded");
-
 // Get elements
 const video = document.getElementById("custom-video-player");
 const playBtn = document.getElementById("play-pause-btn");
@@ -7,30 +5,29 @@ const playImg = document.getElementById("play-pause-img");
 const progressFill = document.getElementById("progress-bar-fill");
 const progressBar = document.querySelector(".progress-bar");
 
-// Play / Pause  with debug
-playBtn.addEventListener("click", async () => {
-  console.log("Play button clicked");
 
-  try {
-    if (video.paused) {
+// Play / Pause 
+playBtn.addEventListener("click", async () => {
+  if (video.paused) {
+    try {
       await video.play();
-      console.log("Video is playing");
       playImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
-    } else {
-      video.pause();
-      console.log("Video paused");
-      playImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+    } catch (err) {
+      console.error("Play failed:", err);
     }
-  } catch (err) {
-    console.error("Play failed:", err);
+  } else {
+    video.pause();
+    playImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
   }
 });
+
 
 // Progress Bar
 video.addEventListener("timeupdate", () => {
   const progress = (video.currentTime / video.duration) * 100;
   progressFill.style.width = progress + "%";
 });
+
 
 // Seeking
 progressBar.addEventListener("click", (e) => {
